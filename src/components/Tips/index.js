58428c89728851fa4a1af4dab3c714201bac9bde
Henrique-Eulalio <henrique.eulalio@sysconecte.com.br>
2020-09-26 14:02:15 -0300

    Sugestions

---
+++ src/components/Tips/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+import img8 from '../../Images/08.png';
+ 
+export default function Tips(){
+    return (
+        <Container>
+            <Option>
+                <Title>Pague suas contas sem sair de casa</Title>
+
+                <Img source={} />
+            </Option>
+        </Container>
+    );
+}
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,6 +1,30 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons';
+
+import { Wrapper, Container, Header, BalanceContainer, BalanceTitle, Balance } from './styles';
+
+import Suggestions from '../../components/Suggestions';
+import Activities from '../../components/Activities';
+import Tips from '../../components/Tips';
 
 export default function Home(){
-    return <Text>Home</Text>;
+    return (
+        <Wrapper>
+            <Container>
+                <Header>
+                    <MaterialCommunityIcons name="qrcode-scan" size={30} color="#43cea2"/>
+
+                    <BalanceContainer>
+                        <BalanceTitle>Meu Saldo</BalanceTitle>
+                        <Balance>R$ 0,00</Balance>
+                    </BalanceContainer>
+
+                    <AntDesign name="gift" size={30} color="#43cea2"/>
+                </Header>
+                <Suggestions />
+                <Activities />
+                <Tips />    
+            </Container>
+        </Wrapper>
+    );
 }
